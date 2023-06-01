281382208370e804bc5b02f837207630a89a6905
Jaeha Lee <wogkdkrm112@gmail.com>
2023-06-01 18:42:14 +0900

    feat: footer 추가

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styles from './footer.module.css';
+import Image from 'next/image';
+
+const Footer = () => {
+  console.log('123');
+
+  return (
+    <div className={styles.container}>
+      <div>©2023 Jaeha. All rights reserved.</div>
+      <div className={styles.social}>
+        <Image
+          src="/1.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="Jaeha Dev Facebook Account"
+        />
+        <Image
+          src="/2.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="Jaeha Dev"
+        />
+        <Image
+          src="/3.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="Jaeha Dev"
+        />
+        <Image
+          src="/4.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="Jaeha Dev"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
